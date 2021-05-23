b35172c8e138d1050443405460d976a61e393cb5
Ivan <ivanbeljaev@mail.ru>
2021-05-23 17:58:21 +0500

    Added PUT

---
+++ Entities/DataTransferObjects/CompanyForUpdateDto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.DataTransferObjects
+{
+    /// <summary>
+    /// Company For Update Dto
+    /// </summary>
+    public class CompanyForUpdateDto
+    {
+        /// <summary>
+        /// Get/sets Name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Get/sets Address
+        /// </summary>
+        public string Address { get; set; }
+
+        /// <summary>
+        /// Get/sets Country
+        /// </summary>
+        public string Country { get; set; }
+
+        /// <summary>
+        /// Get/sets Employees
+        /// </summary>
+        public IEnumerable<EmployeeForCreationDto> Employees { get; set; }
+    }
+}
---
+++ Entities/DataTransferObjects/EmployeeForUpdateDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.DataTransferObjects
+{
+    /// <summary>
+    ///  Employee For Update Dto
+    /// </summary>
+    public class EmployeeForUpdateDto
+    {
+        /// <summary>
+        /// Get/sets Name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Get/sets Age
+        /// </summary>
+        public int Age { get; set; }
+
+        /// <summary>
+        /// Get/sets Position
+        /// </summary>
+        public string Position { get; set; }
+    }
+}
