dde7e1ff0c72bf954f40640ab2f9d820433293c4
Ibelyaev <Ibelyaev@DESKTOP>
2021-05-27 15:21:04 +0500

    Added filtering by age

--- Entities/RequestFeatures/EmployeeParameters.cs
+++ Entities/RequestFeatures/EmployeeParameters.cs
@@ -6,5 +6,19 @@ namespace Entities.RequestFeatures
     /// </summary>
     public class EmployeeParameters : RequestParameters
     {
+        /// <summary>
+        /// Min Age
+        /// </summary>
+        public uint MinAge { get; set; }
+
+        /// <summary>
+        /// Max Age
+        /// </summary>
+        public uint MaxAge { get; set; } = int.MaxValue;
+
+        /// <summary>
+        /// Valid Age Range
+        /// </summary>
+        public bool ValidAgeRange => MaxAge > MinAge;
     }
 }
